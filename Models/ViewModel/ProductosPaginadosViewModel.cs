bde69cd0869a9a39cbf0b4b8a8da0c5d1d98bb67
Estefano Chacin <estefanochacin@wposs.com>
2023-12-13 11:08:11 -0500

    Se actualiza el modelo "ProductosPaginadosViewModel" se le agreagaron mas propiedades

--- Models/ViewModel/ProductosPaginadosViewModel.cs
+++ Models/ViewModel/ProductosPaginadosViewModel.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ANNIE_SHOP.Models.ViewModel
 {
     public class ProductosPaginadosViewModel
     {
-        public int MyProperty { get; set; }
+        public List<Producto> Productos { get; set; } = null!;
+        public int PaginaActual { get; set; }
+        public int TotalPaginas { get; set; }
+        public int? CategoriaIdSeleccionada { get; set; }
+        public string? Busqueda { get; set; }
+        public bool MostrarMensajeSinResultados { get; set; }
+        public string? NombreCategoriaSeleccionada { get; set; }
     }
 }
