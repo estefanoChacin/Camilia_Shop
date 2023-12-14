d3ff5703200801c0b18359a722e0db72833c0ede
Estefano Chacin <estefanochacin@wposs.com>
2023-12-14 11:38:34 -0500

    Se crean los metodos encargados de manejar las excepciones. Excepciones generales, excepciones de BD

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using ANNIE_SHOP.Data;
 using Newtonsoft.Json;
 using ANNIE_SHOP.Models;
+using System.Diagnostics;
+using System.Data.Common;
+using ANNIE_SHOP.Models.ViewModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace ANNIE_SHOP.Controllers
 {
@@ -30,7 +34,7 @@ namespace ANNIE_SHOP.Controllers
         {
             int count = 0;
             string? carritoJson = Request.Cookies["carrito"];
-            if(!string.IsNullOrEmpty(carritoJson))
+            if (!string.IsNullOrEmpty(carritoJson))
             {
                 var carrito = JsonConvert.DeserializeObject<List<ProductoandCantidad>>(carritoJson);
                 if (carrito != null)
@@ -39,6 +43,40 @@ namespace ANNIE_SHOP.Controllers
                 }
             }
             return count;
+        }
+
+
+
+
+        protected IActionResult HandleError(Exception e)
+        {
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        protected IActionResult HandleDbError(DbException dbException)
+        {
+            var viewModel = new DbErrorViewModel
+            {
+                ErrorMessage = "Error de base de datos.",
+                Details = dbException.Message
+            };
+            return View("DbError", viewModel);
+        }
+
+
+
+
+        protected IActionResult HandleDbUpdateError(DbUpdateException dbUpdateError)
+        {
+            var viewModel = new DbErrorViewModel
+            {
+                ErrorMessage = "Error de actualizacion de base de datos.",
+                Details = dbUpdateError.Message
+            };
+            return View("DbError", viewModel);
         }
     }
 }
